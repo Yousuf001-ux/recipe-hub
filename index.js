f2ab9b01c370684c01571cd6ca860307374e49cc
Tobi Awokunle <awokunletobi@gmail.com>
2025-04-22 13:51:28 +0100

    most recent commit

--- index.js
+++ index.js
@@ -79,72 +79,6 @@ function initializeUI(){
                             let detMealName = mealData.meals[0].strMeal
                             let detMealCategory = mealData.meals[0].strCategory
 
-                            // measurements
-                            let measurement1 = mealData.meals[0].strMeasure1
-                            let measurement2 = mealData.meals[0].strMeasure2
-                            let measurement3 = mealData.meals[0].strMeasure3
-                            let measurement4 = mealData.meals[0].strMeasure4
-                            let measurement5 = mealData.meals[0].strMeasure5
-                            let measurement6 = mealData.meals[0].strMeasure6
-                            let measurement7 = mealData.meals[0].strMeasure7
-                            let measurement8 = mealData.meals[0].strMeasure8
-                            let measurement9 = mealData.meals[0].strMeasure9
-                            let measurement10 = mealData.meals[0].strMeasure10
-                            let measurement11 = mealData.meals[0].strMeasure11
-                            let measurement12 = mealData.meals[0].strMeasure12
-                            let measurement13 = mealData.meals[0].strMeasure13
-                            let measurement14 = mealData.meals[0].strMeasure14
-                            let measurement15 = mealData.meals[0].strMeasure15
-                            let measurement16 = mealData.meals[0].strMeasure16
-                            let measurement17 = mealData.meals[0].strMeasure17
-                            let measurement18 = mealData.meals[0].strMeasure18
-                            let measurement19 = mealData.meals[0].strMeasure19
-                            let measurement20 = mealData.meals[0].strMeasure20
-
-                            // Ingredients
-                            let ingdts1 = mealData.meals[0].strIngredient1
-                            let ingdts2 = mealData.meals[0].strIngredient2
-                            let ingdts3 = mealData.meals[0].strIngredient3
-                            let ingdts4 = mealData.meals[0].strIngredient4
-                            let ingdts5 = mealData.meals[0].strIngredient5
-                            let ingdts6 = mealData.meals[0].strIngredient6
-                            let ingdts7 = mealData.meals[0].strIngredient7
-                            let ingdts8 = mealData.meals[0].strIngredient8
-                            let ingdts9 = mealData.meals[0].strIngredient9
-                            let ingdts10 = mealData.meals[0].strIngredient10
-                            let ingdts11 = mealData.meals[0].strIngredient11
-                            let ingdts12 = mealData.meals[0].strIngredient12
-                            let ingdts13 = mealData.meals[0].strIngredient13
-                            let ingdts14 = mealData.meals[0].strIngredient14
-                            let ingdts15 = mealData.meals[0].strIngredient15
-                            let ingdts16 = mealData.meals[0].strIngredient16
-                            let ingdts17 = mealData.meals[0].strIngredient17
-                            let ingdts18 = mealData.meals[0].strIngredient18
-                            let ingdts19 = mealData.meals[0].strIngredient19
-                            let ingdts20 = mealData.meals[0].strIngredient20
-
-                            // Main ingredients
-                            let ingredient1 = `${measurement1} ${ingdts1}`
-                            let ingredient2 = `${measurement2} ${ingdts2}`
-                            let ingredient3 = `${measurement3} ${ingdts3}`
-                            let ingredient4 = `${measurement4} ${ingdts4}`
-                            let ingredient5 = `${measurement5} ${ingdts5}`
-                            let ingredient6 = `${measurement6} ${ingdts6}`
-                            let ingredient7 = `${measurement7} ${ingdts7}`
-                            let ingredient8 = `${measurement8} ${ingdts8}`
-                            let ingredient9 = `${measurement9} ${ingdts9}`
-                            let ingredient10 = `${measurement10} ${ingdts10}`
-                            let ingredient11 = `${measurement11} ${ingdts11}`
-                            let ingredient12 = `${measurement12} ${ingdts12}`
-                            let ingredient13 = `${measurement13} ${ingdts13}`
-                            let ingredient14 = `${measurement14} ${ingdts14}`
-                            let ingredient15 = `${measurement15} ${ingdts15}`
-                            let ingredient16 = `${measurement16} ${ingdts16}`
-                            let ingredient17 = `${measurement17} ${ingdts17}`
-                            let ingredient18 = `${measurement18} ${ingdts18}`
-                            let ingredient19 = `${measurement19} ${ingdts19}`
-                            let ingredient20 = `${measurement20} ${ingdts20}`
-
                             // Instructions
                             let instructions = mealData.meals[0].strInstructions
 
@@ -180,46 +114,16 @@ function initializeUI(){
                             let ingredientText = document.createElement("h1")
                             ingredientText.textContent = "Ingredients"
                             let unorderedList = document.createElement("ul")
-                            let ing1 = document.createElement("li")
-                            ing1.textContent = ingredient1
-                            let ing2 = document.createElement("li")
-                            ing2.textContent = ingredient2
-                            let ing3 = document.createElement("li")
-                            ing3.textContent = ingredient3
-                            let ing4 = document.createElement("li")
-                            ing4.textContent = ingredient4
-                            let ing5 = document.createElement("li")
-                            ing5.textContent = ingredient5
-                            let ing6 = document.createElement("li")
-                            ing6.textContent = ingredient6
-                            let ing7 = document.createElement("li")
-                            ing7.textContent = ingredient7
-                            let ing8 = document.createElement("li")
-                            ing8.textContent = ingredient8
-                            let ing9 = document.createElement("li")
-                            ing9.textContent = ingredient9
-                            let ing10 = document.createElement("li")
-                            ing10.textContent = ingredient10
-                            let ing11 = document.createElement("li")
-                            ing11.textContent = ingredient11
-                            let ing12 = document.createElement("li")
-                            ing12.textContent = ingredient12
-                            let ing13 = document.createElement("li")
-                            ing13.textContent = ingredient13
-                            let ing14 = document.createElement("li")
-                            ing14.textContent = ingredient14
-                            let ing15 = document.createElement("li")
-                            ing15.textContent = ingredient15
-                            let ing16 = document.createElement("li")
-                            ing16.textContent = ingredient16
-                            let ing17 = document.createElement("li")
-                            ing17.textContent = ingredient17
-                            let ing18 = document.createElement("li")
-                            ing18.textContent = ingredient18
-                            let ing19 = document.createElement("li")
-                            ing19.textContent = ingredient19
-                            let ing20 = document.createElement("li")
-                            ing20.textContent = ingredient20
+                            for(let n = 0; n <= 20; n++){
+                                let measurement = mealData.meals[0][`strMeasure${n}`]?.trim()
+                                let ingredient = mealData.meals[0][`strIngredient${n}`]?.trim()
+
+                                if (measurement && ingredient){
+                                    let ingItem = document.createElement("li")
+                                    ingItem.textContent = `${measurement} ${ingredient}`
+                                    unorderedList.appendChild(ingItem)
+                                }
+                            }
                             let instructionsContainer = document.createElement("div")
                             instructionsContainer.classList.add("instructions-container")
                             let instructionTextContainer = document.createElement("div")
@@ -234,7 +138,6 @@ function initializeUI(){
                             instructionTextContainer.append(instructionText)
                             instructionsContainer.append(instructionTextContainer,orderedList)
 
-                            unorderedList.append(ing1,ing2,ing3,ing4,ing5,ing6,ing7,ing8,ing9,ing10,ing11,ing12,ing13,ing14,ing15,ing16,ing17,ing18,ing19,ing20)
                             ingredientTextContainer.append(ingredientText)
                             ingredientsContainer.append(ingredientTextContainer,unorderedList)
                             ingInstContainer.append(ingredientsContainer,instructionsContainer)
@@ -297,6 +200,7 @@ function performSearch(event){
                     let mealName = mealDetails.strMeal
                     let thumbnailImage = mealDetails.strMealThumb
                     let mealCategory = mealDetails.strCategory
+                    let mealID = mealDetails.idMeal
 
                     let card = document.createElement("div")
                     card.classList.add("card")
@@ -330,6 +234,7 @@ function performSearch(event){
 
                     card.addEventListener("click", showDetails)
                     function showDetails(){
+                    details.innerHTML = ""
                     if(heroSection.classList.contains("search-section")){
                         heroSection.classList.remove("search-section")
                         heroSection.classList.add("search-section-hidden")
@@ -343,8 +248,93 @@ function performSearch(event){
                         detailsContainer.classList.add("details-content-visible")
                     }
 
+                    if(mealID){
+                        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`).then((response)=>{
+                            return response.json()
+                        }).then((mealData)=>{
+                            console.log(mealData);
+
+                            let bigThumbnail= mealData.meals[0].strMealThumb
+                            let detMealName = mealData.meals[0].strMeal
+                            let detMealCategory = mealData.meals[0].strCategory
+
+                            // Instructions
+                            let instructions = mealData.meals[0].strInstructions
+
+                            let thumbnailContentContainer = document.createElement("div")
+                            thumbnailContentContainer.classList.add("thumbnail-big-content")
+                            let bThumbnailContainer = document.createElement("div")
+                            bThumbnailContainer.classList.add("thumbnail-big")
+                            let thumbnailBig = document.createElement("img")
+                            thumbnailBig.setAttribute("src", `${bigThumbnail}`)
+                            let contentContainer = document.createElement("div")
+                            contentContainer.classList.add("content")
+                            let mealNameTimeContainer = document.createElement("div")
+                            mealNameTimeContainer.classList.add("meal-name-time")
+                            let mealNameContainer = document.createElement("div")
+                            mealNameContainer.classList.add("meal-name-d")
+                            let dMealName = document.createElement("h1")
+                            dMealName.textContent = detMealName
+                            let prepTimeContainer = document.createElement("div")
+                            prepTimeContainer.classList.add("prep-time")
+                            let paragraph = document.createElement("p")
+                            paragraph.textContent = "Approximately 30 mins to prepare"    
+                            let categoryContainer = document.createElement("div")
+                            categoryContainer.classList.add("category")
+                            let categoryParagraph = document.createElement("p")
+                            categoryParagraph.textContent = detMealCategory
+
+                            let ingInstContainer = document.createElement("div")
+                            ingInstContainer.classList.add("ingd-inst")
+                            let ingredientsContainer = document.createElement("div")
+                            ingredientsContainer.classList.add("ingredients-container")
+                            let ingredientTextContainer = document.createElement("div")
+                            ingredientTextContainer.classList.add("ingredients-text")
+                            let ingredientText = document.createElement("h1")
+                            ingredientText.textContent = "Ingredients"
+                            let unorderedList = document.createElement("ul")
+                            for(let n = 0; n <= 20; n++){
+                                let measurement = mealData.meals[0][`strMeasure${n}`]?.trim()
+                                let ingredient = mealData.meals[0][`strIngredient${n}`]?.trim()
+
+                                if (measurement && ingredient){
+                                    let ingItem = document.createElement("li")
+                                    ingItem.textContent = `${measurement} ${ingredient}`
+                                    unorderedList.appendChild(ingItem)
+                                }
+                            }
+                            let instructionsContainer = document.createElement("div")
+                            instructionsContainer.classList.add("instructions-container")
+                            let instructionTextContainer = document.createElement("div")
+                            instructionTextContainer.classList.add("instructions-text")
+                            let instructionText = document.createElement("h1")
+                            instructionText.textContent = "Instructions"
+                            let orderedList = document.createElement("ol")
+                            let inst = document.createElement("li")
+                            inst.textContent = instructions
+
+                            orderedList.append(inst)
+                            instructionTextContainer.append(instructionText)
+                            instructionsContainer.append(instructionTextContainer,orderedList)
+
+                            ingredientTextContainer.append(ingredientText)
+                            ingredientsContainer.append(ingredientTextContainer,unorderedList)
+                            ingInstContainer.append(ingredientsContainer,instructionsContainer)
+
+                            categoryContainer.append(categoryParagraph)
+                            prepTimeContainer.append(paragraph)
+                            mealNameContainer.append(dMealName)
+                            mealNameTimeContainer.append(mealNameContainer,prepTimeContainer)
+                            contentContainer.append(mealNameTimeContainer,categoryContainer)
+                            bThumbnailContainer.append(thumbnailBig)
+                            thumbnailContentContainer.append(bThumbnailContainer,contentContainer)
+                            details.classList.add("details")
+                            details.append(thumbnailContentContainer,ingInstContainer)
+                        })
+                    }
+
                     backButton.addEventListener("click", goBack)
-                function goBack(){
+                    function goBack(){
                     if(heroSection.classList.contains("search-section-hidden")){
                         heroSection.classList.remove("search-section-hidden")
                         heroSection.classList.add("search-section")
